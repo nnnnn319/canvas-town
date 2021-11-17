22c0d5e85fb169ba44567c70eef05daf8e943290
yaomenglong <745998202@qq.com>
2021-11-17 09:46:46 +0800

    delete node_modules

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+
+router.get('/registerName', function(req, res, next){
+  
+})
 module.exports = router;
