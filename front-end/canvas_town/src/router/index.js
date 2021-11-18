a30fd8a7d49da0ac55bed42f111bcc41039b6c1a
yaomenglong <745998202@qq.com>
2021-11-18 16:51:31 +0800

    添加gameHome权限

--- front-end/canvas_town/src/router/index.js
+++ front-end/canvas_town/src/router/index.js
@@ -27,7 +27,7 @@ export default new VueRouter({
             path: '/gameHome',
             component: () => import(/* webpackChunkName: "pageHome" */ '../components/page/gameHome.vue'),
             // 设置相应元信息
-            meta: { title: 'gameHome组件' },
+            meta: { title: 'gameHome组件', requireAuth: true},
         }
     ]
 })
